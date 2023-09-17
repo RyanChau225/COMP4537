3b399aa5b5ba4c14a8bd83eea75f0b21755765b1
spicious225 <ryanthelion.c@gmail.com>
2023-09-17 01:07:37 -0700

    finish again

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the "public" directory under the "/lab1/" path
+app.get('/', (req, res) => {
+  res.redirect('/lab1/index.html');
+});
+
+
+app.use('/lab1/', express.static(path.join(__dirname, 'public')));
+
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
